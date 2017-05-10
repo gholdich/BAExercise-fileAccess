77e9ebccd6d43f8e4e098f94b1aedc931ab2b62f
Ubuntu VM <gholdich@outlook.com>
2017-05-10 10:11:18 +0100

    Ubuntu VM: Second commit

--- Person.java
+++ Person.java
@@ -24,39 +24,30 @@ public class Person {
 	public String getName() {
 		return name;
 	}
-
 	public String getDob() {
 		return dob;
 	}
-
 	public String getOccupation() {
 		return occupation;
 	}
-
 	public int getSalary() {
 		return salary;
 	}
-
 	public ArrayList<Animal> getPets() {
 		return pets;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public void setDob(String dob) {
 		this.dob = dob;
 	}
-
 	public void setOccupation(String occupation) {
 		this.occupation = occupation;
 	}
-
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
-
 	public void setPets(ArrayList<Animal> pets) {
 		this.pets = pets;
 	}
